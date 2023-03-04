e8581728a04a082a3f927d5688b3cf7b0498ed87
Nabil Hadi <napilfadl@gmail.com>
2023-03-04 11:20:14 +0200

    Code refactor

--- dynamic-ui.js
+++ dynamic-ui.js
@@ -1,82 +1,224 @@
-const DropMenuClick = (function () {
-  function use(
-    dropMenuItemHideClass,
-    {
-      dropMenuParentSelector = ".drop-menu-click",
-      dropMenuTitleSelector = ".drop-menu-title",
-      dropMenuItemsContainerSelector = ".drop-menu-items-container",
-    } = {}
-  ) {
-    const allDropMenuClick = document.querySelectorAll(dropMenuParentSelector);
-    allDropMenuClick.forEach((dropMenu) => {
-      const dropMenuTitle = dropMenu.querySelector(dropMenuTitleSelector);
-      const dropMenuItemContainer = dropMenu.querySelector(
-        dropMenuItemsContainerSelector
-      );
+const hideClass = "nh-dynamic-ui-hide";
+createCSSSelector("." + hideClass, "display: none");
 
-      if (!dropMenuTitle || !dropMenuItemContainer) return;
+const dropDownMenusClick = document.querySelectorAll(".drop-menu-click");
+dropDownMenusClick.forEach((menu) => {
+  const menuTitle = menu.querySelector(".drop-menu-title");
+  const menuItems = menu.querySelectorAll(".drop-menu-item");
 
-      if (!dropMenuItemHideClass) dropMenuItemContainer.style.display = "none";
-      else dropMenuItemContainer.classList.add(dropMenuItemHideClass);
+  menuItems.forEach((item) => {
+    item.classList.add(hideClass);
+  });
 
-      dropMenuTitle.addEventListener("click", (event) => {
-        event.stopPropagation();
-
-        if (!dropMenuItemHideClass) {
-          if (dropMenuItemContainer.style.display === "none")
-            dropMenuItemContainer.style.display = "block";
-          else dropMenuItemContainer.style.display = "none";
-        } else dropMenuItemContainer.classList.toggle(dropMenuItemHideClass);
-      });
+  let isShown = false;
+  menuTitle.addEventListener("click", () => {
+    menuItems.forEach((item) => {
+      if (isShown) {
+        item.classList.add(hideClass);
+      } else {
+        item.classList.remove(hideClass);
+      }
     });
+    isShown = !isShown;
+  });
+});
+
+const dropDownMenusHover = document.querySelectorAll(".drop-menu-hover");
+dropDownMenusHover.forEach((menu) => {
+  const menuItems = menu.querySelectorAll(".drop-menu-item");
+
+  menuItems.forEach((item) => {
+    item.classList.add(hideClass);
+  });
+
+  menu.addEventListener("mouseenter", () => {
+    menuItems.forEach((item) => item.classList.remove(hideClass));
+  });
+
+  menu.addEventListener("mouseleave", () => {
+    menuItems.forEach((item) => item.classList.add(hideClass));
+  });
+});
+
+const imageCarouselProto = {
+  getNextNumberInRange() {
+    const { max, currentImageNumber: current } = this;
+    if (current < 0 || current + 1 > max) {
+      return 0;
+    } else {
+      return current + 1;
+    }
+  },
+
+  getPreviousNumberInRange() {
+    const { max, currentImageNumber: current } = this;
+    if (current < 0 || current - 1 < 0) {
+      return max;
+    } else {
+      return current - 1;
+    }
+  },
+  advance(advanceNumberFn) {
+    const { imageCarouselItems } = this;
+    this.removeActiveClass();
+    imageCarouselItems[this.currentImageNumber].classList.add(hideClass);
+    this.currentImageNumber = advanceNumberFn.call(this);
+    imageCarouselItems[this.currentImageNumber].classList.remove(hideClass);
+    this.setActiveClass();
+    this.setUpTimeout();
+  },
+  next() {
+    this.advance(this.getNextNumberInRange);
+  },
+  previous() {
+    this.advance(this.getPreviousNumberInRange);
+  },
+  goto(index) {
+    this.advance(() => Number(index));
+  },
+  setUpTimeout() {
+    clearTimeout(this.timeoutId);
+    this.timeoutId = setTimeout(() => {
+      this.next();
+    }, 2000);
+  },
+  setActiveClass() {
+    const dot = this.navigationDotsList.querySelector(
+      `[data-index="${this.currentImageNumber}"]`
+    );
+    dot.classList.add("active");
+  },
+  removeActiveClass() {
+    const dot = this.navigationDotsList.querySelector(
+      `[data-index="${this.currentImageNumber}"]`
+    );
+    dot.classList.remove("active");
+  },
+};
+
+function createImageCarousel(imageCarouselContainer) {
+  const obj = {};
+  obj.imageCarouselItems = imageCarouselContainer.querySelectorAll(
+    ".image-carousel-item"
+  );
+
+  obj.max = obj.imageCarouselItems.length - 1;
+
+  obj.nextBtn = imageCarouselContainer.querySelector(
+    ".image-carousel-next-btn"
+  );
+  obj.previousBtn = imageCarouselContainer.querySelector(
+    ".image-carousel-previous-btn"
+  );
+  obj.nextBtn.addEventListener("click", () => {
+    obj.next();
+  });
+  obj.previousBtn.addEventListener("click", () => {
+    obj.previous();
+  });
+
+  obj.currentImageNumber = 0;
+  obj.imageCarouselItems.forEach((item, key) => {
+    if (key === obj.currentImageNumber) return;
+    item.classList.add(hideClass);
+  });
+
+  obj.navigationDotsList = imageCarouselContainer.querySelector(
+    ".image-carousel-dots-list"
+  );
+  obj.imageCarouselItems.forEach((item, key) => {
+    obj.navigationDotsList.innerHTML += `<div class="image-carousel-dot" data-index="${key}"></div>`;
+  });
+  obj.navigationDotsList.addEventListener("click", (event) => {
+    const index = event.target.dataset.index;
+    if (index == undefined) return;
+
+    obj.goto(index);
+  });
+
+  obj.timeoutId = null;
+  return Object.assign(obj, imageCarouselProto);
+}
+
+const imageCarousels = Array.from(
+  document.querySelectorAll(".image-carousel-container")
+)
+  .map(createImageCarousel)
+  .forEach((caruousel) => {
+    caruousel.setUpTimeout();
+    caruousel.setActiveClass();
+  });
+
+// Function to create CSS classes using JavaScript
+// I used it to create a "hide" class to hide menu items
+// Original SOW answer: https://stackoverflow.com/a/8630641/8327347
+function createCSSSelector(selector, style) {
+  if (!document.styleSheets) return;
+  if (document.getElementsByTagName("head").length == 0) return;
+
+  var styleSheet, mediaType;
+
+  if (document.styleSheets.length > 0) {
+    for (var i = 0, l = document.styleSheets.length; i < l; i++) {
+      if (document.styleSheets[i].disabled) continue;
+      var media = document.styleSheets[i].media;
+      mediaType = typeof media;
+
+      if (mediaType === "string") {
+        if (media === "" || media.indexOf("screen") !== -1) {
+          styleSheet = document.styleSheets[i];
+        }
+      } else if (mediaType == "object") {
+        if (
+          media.mediaText === "" ||
+          media.mediaText.indexOf("screen") !== -1
+        ) {
+          styleSheet = document.styleSheets[i];
+        }
+      }
+
+      if (typeof styleSheet !== "undefined") break;
+    }
   }
 
-  return {
-    use,
-  };
-})();
+  if (typeof styleSheet === "undefined") {
+    var styleSheetElement = document.createElement("style");
+    styleSheetElement.type = "text/css";
+    document.getElementsByTagName("head")[0].appendChild(styleSheetElement);
 
-const DropMenuHover = (function () {
-  function use(
-    dropMenuItemHideClass,
-    {
-      dropMenuParentSelector = ".drop-menu-hover",
-      dropMenuTitleSelector = ".drop-menu-title",
-      dropMenuItemsContainerSelector = ".drop-menu-items-container",
-    } = {}
-  ) {
-    const allDropMenuHover = document.querySelectorAll(dropMenuParentSelector);
-    allDropMenuHover.forEach((dropMenu) => {
-      const dropMenuTitle = dropMenu.querySelector(dropMenuTitleSelector);
-      const dropMenuItemContainer = dropMenu.querySelector(
-        dropMenuItemsContainerSelector
-      );
+    for (i = 0; i < document.styleSheets.length; i++) {
+      if (document.styleSheets[i].disabled) {
+        continue;
+      }
+      styleSheet = document.styleSheets[i];
+    }
 
-      if (!dropMenuTitle || !dropMenuItemContainer) return;
-
-      if (!dropMenuItemHideClass) dropMenuItemContainer.style.display = "none";
-      else dropMenuItemContainer.classList.add(dropMenuItemHideClass);
-
-      dropMenu.style.position = "relative";
-      dropMenuItemContainer.style.position = "absolute";
-
-      dropMenuTitle.addEventListener("mouseenter", () => {
-        if (!dropMenuItemHideClass)
-          dropMenuItemContainer.style.display = "block";
-        else dropMenuItemContainer.classList.remove(dropMenuItemHideClass);
-      });
-
-      dropMenu.addEventListener("mouseleave", () => {
-        if (!dropMenuItemHideClass)
-          dropMenuItemContainer.style.display = "none";
-        else dropMenuItemContainer.classList.add(dropMenuItemHideClass);
-      });
-    });
+    mediaType = typeof styleSheet.media;
   }
 
-  return {
-    use,
-  };
-})();
-
-export { DropMenuClick, DropMenuHover };
+  if (mediaType === "string") {
+    for (var i = 0, l = styleSheet.rules.length; i < l; i++) {
+      if (
+        styleSheet.rules[i].selectorText &&
+        styleSheet.rules[i].selectorText.toLowerCase() == selector.toLowerCase()
+      ) {
+        styleSheet.rules[i].style.cssText = style;
+        return;
+      }
+    }
+    styleSheet.addRule(selector, style);
+  } else if (mediaType === "object") {
+    var styleSheetLength = styleSheet.cssRules ? styleSheet.cssRules.length : 0;
+    for (var i = 0; i < styleSheetLength; i++) {
+      if (
+        styleSheet.cssRules[i].selectorText &&
+        styleSheet.cssRules[i].selectorText.toLowerCase() ==
+          selector.toLowerCase()
+      ) {
+        styleSheet.cssRules[i].style.cssText = style;
+        return;
+      }
+    }
+    styleSheet.insertRule(selector + "{" + style + "}", styleSheetLength);
+  }
+}
